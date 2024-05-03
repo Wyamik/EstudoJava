00ad08805dff5a0a465c202d4769b623dd354047
Raphael Siqueira <raphael.siqueira4@gmail.com>
2024-05-03 20:53:20 -0300

    exercicioZero

---
+++ variaveis/exercicioZero.java
@@ -0,0 +1,12 @@
+package variaveis;
+
+public class exercicioZero {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello World!");
+
+
+    }
+
+}
